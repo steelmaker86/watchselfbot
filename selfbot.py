9801cd0fced361cd611d585be16d5129b1cd1bfd
Steelmaker86 <Steelmaker86@gmail.com>
2017-02-20 10:00:04 -0600

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -6,7 +6,7 @@ from io import BytesIO, StringIO
 import time
 
 client = discord.Client()
-VERSION = '0.0.2.'
+VERSION = '0.0.3.'
 
 @client.event
 async def on_ready():
@@ -30,14 +30,47 @@ async def on_message(message):
 		await reply(message, 'turning off...')
 		await client.logout()
 	elif command.startswith('+time'):
-		await reply(message, 'The date and time is: ' + time.ctime())
+		await reply(message, 'The time and date is: ' + time.ctime())
 	elif command.startswith('+info'):
 		await reply(message, 'I am watchbot, a discord selfbot made by Nukekin that tells the time, and I also have other random stuff. I am on version: ' + VERSION + ' I was made in python. I was also inspired by the selfbot [RDT]Test made!! find me at: https://github.com/Steelmaker86/watchselfbot !')
 	elif command.startswith('+antigrav'):
 		await reply(message, 'imported the dopest antigravity memz 100%')
 		import antigravity
-		
-		##async def reply(message, text):
+    elif command.startswith('+eval'):
+        output = None
+        if parameters == '':
+            await reply(message, '```enter a string of code to evaluate!```')
+            return
+        try:
+            output = eval(parameters)
+        except:
+            await reply(message, '```\n' + str(traceback.format_exc()) + '\n```')
+            traceback.print_exc()
+        if asyncio.iscoroutine(output):
+            output = await output
+        if output:
+            await reply(message, '```\n' + str(output) + '\n```')
+    elif command.startswith('+exec'):
+        if parameters == '':
+            await reply(message, '```enter a string of code to execute!```')
+            return
+        old_stdout = sys.stdout
+        redirected_output = sys.stdout = StringIO()
+        try:
+            exec(parameters)
+        except Exception:
+            formatted_lines = traceback.format_exc().splitlines()
+            await reply(message, '```py\n{}\n{}\n```'.format(formatted_lines[-1], '\n'.join(formatted_lines[4:-1])))
+            return
+        finally:
+            sys.stdout = old_stdout
+
+        if redirected_output.getvalue():
+            await reply(message, redirected_output.getvalue())
+            return
+        await reply(message, 'successfully executed code!')
+        return
+##async def reply(message, text):
 ##	await client.send_message(message.channel, message.author.mention + ', ' + text)
 async def reply(message, text):
 	await client.edit_message(message, message.author.mention + ', ' + text)
