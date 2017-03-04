e5be506b1e99a142f8e84e18269d1659bce098fa
Steelmaker86 <Steelmaker86@gmail.com>
2017-03-04 11:26:33 -0600

    Update and rename stuff.bat to runbot.bat

--- runbot.bat
+++ runbot.bat
@@ -6,6 +6,6 @@ pushd %~dp0
 ::Attempts to start py launcher without relying on PATH
 %SYSTEMROOT%\py.exe --version > NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 GOTO attempt
-%SYSTEMROOT%\py.exe -3 testrun.py
+%SYSTEMROOT%\py.exe -3 bot.py
 PAUSE
 GOTO end
