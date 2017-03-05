86c7e1f9132910af61cca07d1301d8e51728b34e
Steelmaker86 <Steelmaker86@gmail.com>
2017-03-04 18:10:32 -0600

    Major New code
    Added a richembed to +info!

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord import *
 import asyncio
 import traceback
 import sys
@@ -6,9 +7,15 @@ from io import BytesIO, StringIO
 import time
 import config
 
+embed = discord.embeds.Embed(colour=0xff0000)
+VERSION = '0.0.5.'
+about = 'I am watchbot, a discord selfbot made by Nukekin that tells the time, and I also have other random stuff. I am on version: ' + VERSION +  ' I was made in python. I was also inspired by the selfbot [RDT]Test made!! find me at: https://github.com/Steelmaker86/watchselfbot !'
 client = discord.Client()
-VERSION = '0.0.4.'
 prefix = config.prefix
+embed.add_field(name="Language", value="Python", inline=True)
+embed.add_field(name="Api Wrapper", value="Discord.py", inline=True)
+embed.add_field(name="Version", value="0.0.5", inline=True)
+embed.add_field(name="About Me", value=about, inline=False)
 
 @client.event
 async def on_ready():
@@ -24,17 +31,21 @@ async def on_ready():
 async def on_message(message):
 	if message.author.id != client.user.id:
 		return
-	if message.content.startswith('config.prefix'):
+	if message.content.startswith('+'):
 		print('Command: ' + message.content)
 	command = message.content
 	parameters = ' '.join(message.content.strip().split(' ')[1:])
-	if command.startswith(prefix +"shutdown"):
+	if command.startswith(prefix + "shutdown"):
 		await reply(message, 'turning off...')
 		await client.logout()
 	elif command.startswith(prefix + 'time'):
 		await reply(message, 'The time and date is: ' + time.ctime())
 	elif command.startswith(prefix +'info'):
-		await reply(message, 'I am watchbot, a discord selfbot made by Nukekin that tells the time, and I also have other random stuff. I am on version: ' + VERSION + ' I was made in python. I was also inspired by the selfbot [RDT]Test made!! find me at: https://github.com/Steelmaker86/watchselfbot !')
+		try:
+			await client.edit_message(message, embed = embed)
+		except discord.HTTPException:
+			await reply("I need the `Embed links` permission "
+                               "to send this")
 	elif command.startswith(prefix + 'antigrav'):
 		await reply(message, 'importing the dopest antigravity memz 35%')
 		time.sleep(2)
@@ -51,5 +62,6 @@ async def on_message(message):
 
 async def reply(message, text):
 	await client.edit_message(message, text)
-
+async def repost(message, text):
+    await client.send_message(message, text)
 client.run(config.email, config.password)
