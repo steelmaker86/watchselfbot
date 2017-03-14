bc577295b60574bf2a72f301e86417b0bba160ea
Steelmaker86 <Steelmaker86@gmail.com>
2017-03-13 20:33:22 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,21 +9,23 @@ import config
 
 embed = discord.embeds.Embed(colour=0xff0000)
 VERSION = '0.0.5.'
-about = 'I am watchbot, a discord selfbot made by Nukekin that tells the time, and I also have other random stuff. I am on version: ' + VERSION +  ' I was made in python. I was also inspired by the selfbot [RDT]Test made!! find me at: https://github.com/Steelmaker86/watchselfbot !'
+about = 'I am watchbot, a discord selfbot made by <@212589934966472704> that tells the time, and I also have other random stuff. I am on version: ' + VERSION +  ' I was made in python. I was also inspired by the selfbot [RDT]Test made!! find me at: https://github.com/Steelmaker86/watchselfbot !'
 client = discord.Client()
 prefix = config.prefix
 embed.add_field(name="Language", value="Python", inline=True)
 embed.add_field(name="Api Wrapper", value="Discord.py", inline=True)
 embed.add_field(name="Version", value="0.0.5", inline=True)
 embed.add_field(name="About Me", value=about, inline=False)
-
+embed.set_footer(text="Made to help discordians since 2-19-2017!")
 @client.event
 async def on_ready():
-	print('signed in as:')
+	startup = time.ctime()
+	print('Signed in as:')
 	print(client.user.name)
 	print(client.user.id)
 	print('~~~~~~~~~~~')
 	print(client)
+	print('Startup at: ' + startup)
 
 	
 
@@ -44,7 +46,7 @@ async def on_message(message):
 		try:
 			await client.edit_message(message, embed = embed)
 		except discord.HTTPException:
-			await reply("I need the `Embed links` permission "
+			await reply(message, "I need the `Embed links` permission "
                                "to send this")
 	elif command.startswith(prefix + 'antigrav'):
 		await reply(message, 'importing the dopest antigravity memz 35%')
@@ -55,7 +57,12 @@ async def on_message(message):
 		import antigravity
 	elif command.startswith(prefix + 'lenny'):
 		await reply(message, '( ͡° ͜ʖ ͡°)')
-
+	elif command.startswith(prefix + 'help'):
+		await client.edit_message(message, 'Here are my commands, If you need some info:\n' 
+		 'go to the readme.md file.\n'
+		 'My commands are: antigrav, help, info,\n' 
+		 'time, shutdown, and lenny\n')
+		
 		##async def reply(message, text):
 ##	await client.send_message(message.channel, message.author.mention + ', ' + text)
 
