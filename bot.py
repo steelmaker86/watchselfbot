19f4165eeb1f4309fc6e5e444072536e5dba9b47
Steelmaker86 <Steelmaker86@gmail.com>
2017-03-17 11:51:05 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,23 +7,17 @@ from io import BytesIO, StringIO
 import time
 import config
 
+client = discord.Client()
 author_repo = 'https://github.com/steelmaker86/watchselfbot'
 embed = discord.embeds.Embed(colour=0xff0000)
 VERSION = '0.0.7'
 about = (
 "I am watchbot, a discord selfbot made by <@212589934966472704> that tells the\n"
-"time, and I also have other random stuff. I am on version: " + VERSION + 
-". I was made in python. I was also inspired by the selfbot [RDT]Test made!! You can find me [HERE!]({})\n"
+"time, and I also have other random stuff. I am currently on\n"
+"version: " + VERSION + ". I was made in python. I was also inspired by the\n"
+"selfbot [RDT]Test made!! You can find me [HERE!]({})\n"
 "".format(author_repo))
-client = discord.Client()
 prefix = config.prefix
-embed.add_field(name="Language", value="Python", inline=True)
-embed.add_field(name="Api Wrapper", value="Discord.py", inline=True)
-embed.add_field(name="Version", value=VERSION, inline=True)
-embed.add_field(name="Creator", value="Nukeking", inline=True)
-embed.add_field(name="About Me", value=about, inline=False)
-embed.set_footer(text="Made to help discordians since 2-19-2017!")
-embed.set_thumbnail(url='http://i.picresize.com/images/2017/03/15/TlYWM.png')
 
 
 @client.event
@@ -35,7 +29,15 @@ async def on_ready():
 	print('~~~~~~~~~~~')
 	print(client)
 	print('Startup at: ' + startup)
-
+	user = client.user.name
+	embed.set_author(name='Watchselfbot', url='http://github.com/steelmaker86/watchselfbot', icon_url=Embed.Empty)
+	embed.add_field(name="Instance owned by:", value=user, inline=True)
+	embed.add_field(name="Language", value="Python", inline=True)
+	embed.add_field(name="Api Wrapper", value="Discord.py", inline=True)
+	embed.add_field(name="Version", value=VERSION, inline=True)
+	embed.add_field(name="About Me", value=about, inline=False)
+	embed.set_footer(text="Made to help discordians since 2-19-2017!")
+	embed.set_thumbnail(url='http://i.picresize.com/images/2017/03/15/TlYWM.png')
 
 
 @client.event
@@ -76,5 +78,7 @@ async def on_message(message):
 		await client.edit_message(message, "I have an apple. I have a pen. UH! Apple Pen!\n"
 		"I have a pineapple. I have a pen. UH! pineapple Pen!\n"
 		"Apple Pen, Pineapple Pen. UH! Pen Pineapple apple pen!\n")
+	elif command.startswith(prefix + 'communism'):
+		await client.edit_message(message, 'Communism is annoying, but here you go... ☭')
 
 client.run(config.email, config.password)
